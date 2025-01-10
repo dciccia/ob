f9a538a802d07083850a3fa197a4595b416af2b0
Diego Ciccia <ciccia.d@gmail.com>
2025-01-09 21:00:53 -0500

    added before hook and one more test

--- tests/main.landing.spec.ts
+++ tests/main.landing.spec.ts
@@ -1,15 +1,16 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://www.onebrief.com/');
+test.beforeEach(async ({ page }) => {
+  // Go to the starting url before each test.
+  await page.goto('https://onebrief.com/');
+});
 
+test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Onebrief | Home/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://www.onebrief.com/');
-
   // Click the get started button.
   await page.locator(".nav_cta-wrap").click();
 
@@ -21,4 +22,18 @@ test('get started link', async ({ page }) => {
 
   //Expect page to have some text
   await expect(page.getByText("Getting started shouldn’t be complex.")).toBeVisible();
+});
+
+test('see the platform', async ({ page }) => {
+  //Click see the platform button
+  await page.getByText("See the Platform").click();
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Platform | Onebrief/);
+
+  //Expect URL to contain page path
+  await expect(page).toHaveURL(/platform/);
+
+  //Expect page to have some text
+  await expect(page.getByText("Before we built Onebrief, we lived your problems.")).toBeVisible();
 });
